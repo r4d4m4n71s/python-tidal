7cd9b7e978a3d3dc08c08ee50b441c7b47738335
tehkillerbee <tehkillerbee@users.noreply.github.com>
2025-08-19 15:05:45 +0200

    Workers: Use get_*_count to get the actual number of items.

--- tidalapi/workers.py
+++ tidalapi/workers.py
@@ -6,18 +6,30 @@ log = logging.getLogger(__name__)
 
 
 def func_wrapper(args):
-    (f, offset, *args) = args
+    """
+    args: tuple(func, limit, offset, *extra_args)
+    Returns list of (index, item)
+    """
+    func, limit, offset, *extra_args = args
     try:
-        items = f(*args)
+        items = func(limit, offset, *extra_args)
     except Exception as e:
-        log.error("Failed to run %s(offset=%d, args=%s)", f, offset, args)
+        log.error(
+            "Failed to run %s(limit=%d, offset=%d, args=%s)",
+            func,
+            limit,
+            offset,
+            extra_args,
+        )
         log.exception(e)
         items = []
-    return list((i + offset, item) for i, item in enumerate(items))
+
+    return [(i + offset, item) for i, item in enumerate(items)]
 
 
 def get_items(
     func: Callable,
+    total_count: int,
     *args,
     parse: Callable = lambda _: _,
     chunk_size: int = 50,
@@ -25,40 +37,14 @@ def get_items(
 ):
     """This function performs pagination on a function that supports `limit`/`offset`
     parameters and it runs API requests in parallel to speed things up."""
+    offsets = list(range(0, total_count, chunk_size))
     items = []
-    offsets = [-chunk_size]
-    remaining = chunk_size * processes
 
-    with ThreadPoolExecutor(
-        processes, thread_name_prefix=f"mopidy-tidal-{func.__name__}-"
-    ) as pool:
-        while remaining == chunk_size * processes:
-            offsets = [offsets[-1] + chunk_size * (i + 1) for i in range(processes)]
+    with ThreadPoolExecutor(processes) as pool:
+        args_list = [(func, chunk_size, offset, *args) for offset in offsets]
 
-            pool_results = pool.map(
-                func_wrapper,
-                [
-                    (
-                        func,
-                        offset,
-                        chunk_size,  # limit
-                        offset,  # offset
-                        *args,  # extra args (e.g. order, order_direction)
-                    )
-                    for offset in offsets
-                ],
-            )
+        for page_items in pool.map(func_wrapper, args_list):
+            items.extend(page_items)
 
-            new_items = []
-            for results in pool_results:
-                new_items.extend(results)
-
-            remaining = len(new_items)
-            items.extend(new_items)
-
-    items = [_ for _ in items if _]
-    sorted_items = list(
-        map(lambda item: item[1], sorted(items, key=lambda item: item[0]))
-    )
-
-    return list(map(parse, sorted_items))
+    items = [item for _, item in sorted(items, key=lambda x: x[0])]
+    return list(map(parse, items))
